e86fe7ae030ef1ac8a4fd8ae5bfa74ba66b72ff8
Hassan <hboulhilt@gmail.com>
2023-03-22 02:45:59 +0000

    same api methods on article model functionality accomplishment with API_VIEW class

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view, article_detail_api_view
+# from news.api.views import article_list_create_api_view, article_detail_api_view
+from news.api.views import Article_List_Create_API_View, Article_Detail_API_View
 
 urlpatterns =[
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>/", article_detail_api_view, name="article-detail"),
+    path("articles/", Article_List_Create_API_View.as_view(), name="article-list"),
+    path("articles/<int:pk>/", Article_Detail_API_View.as_view(), name="article-detail"),
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>/", article_detail_api_view, name="article-detail"),
+  
 ]
--- news/api/views.py
+++ news/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from news.models import Article
 from news.api.serializers import ArticleSerializer
@@ -37,3 +38,35 @@ def article_detail_api_view(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
     
+class Article_List_Create_API_View(APIView):
+    def get(self, request):
+        articles = Article.objects.filter(active=True)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Article_Detail_API_View(APIView):
+    def get_object(self, pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            return Response({"error": {"code": 404, "message": "Article not found!"}}, status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
