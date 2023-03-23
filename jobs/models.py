026a80c106a9632a0f931baa883b6b8b9eaa87b1
Hassan <hboulhilt@gmail.com>
2023-03-23 23:59:34 +0000

    create job app and it's model

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class JobOffer(models.Model):
+    job_company = models.CharField(max_length=120)
+    job_email = models.EmailField()
+    job_title = models.CharField(max_length=120)
+    job_description = models.TextField()
+    job_location = models.CharField(max_length=120)
+    job_salary = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    job_avalaible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} {self.location}"
