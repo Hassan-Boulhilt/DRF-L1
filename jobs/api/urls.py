ef87bb876e31cf215de4a40c965061fc45c311ad
Hassan <hboulhilt@gmail.com>
2023-03-24 00:30:57 +0000

    add url path for job details

--- jobs/api/urls.py
+++ jobs/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from jobs.api.views import JobOffer_List_Create_API_View
+from jobs.api.views import JobOffer_List_Create_API_View, JobOffer_Detail_API_View
 
 
 urlpatterns = [
     path("job-offers/", JobOffer_List_Create_API_View.as_view(), name="job-offers"),
+    path("job-offers/<int:pk>/", JobOffer_Detail_API_View.as_view(), name="job-offer-detail"),
 ]
